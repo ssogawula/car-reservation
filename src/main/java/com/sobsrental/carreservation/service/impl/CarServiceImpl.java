0af4758054acf940ae0c9781b846ef1b4f52aa24
Sonwabile Sogawula <SonwabileS@SARATOGA.LOCAL>
2020-03-20 23:11:40 +0200

    CarService Test added getCarByName_ShouldReturnCarDetails

--- src/main/java/com/sobsrental/carreservation/service/impl/CarServiceImpl.java
+++ src/main/java/com/sobsrental/carreservation/service/impl/CarServiceImpl.java
@@ -3,15 +3,21 @@ package com.sobsrental.carreservation.service.impl;
 import org.springframework.stereotype.Service;
 
 import com.sobsrental.carreservation.domain.Car;
+import com.sobsrental.carreservation.repository.CarRepository;
 import com.sobsrental.carreservation.service.CarService;
 
 @Service
 public class CarServiceImpl implements CarService {
-
+	
+	private final CarRepository carRepository;
+	
+	public CarServiceImpl(CarRepository carRepository) {
+		this.carRepository = carRepository;
+	}
+	
 	@Override
 	public Car getCarByCarName(String carName) {
-		// TODO Auto-generated method stub
-		return null;
+		return carRepository.findByCarName(carName);
 	}
 
 }
--- src/test/java/com/sobsrental/carreservation/service/CarServiceTest.java
+++ src/test/java/com/sobsrental/carreservation/service/CarServiceTest.java
@@ -1,5 +1,36 @@
 package com.sobsrental.carreservation.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.sobsrental.carreservation.domain.Car;
+import com.sobsrental.carreservation.repository.CarRepository;
+import com.sobsrental.carreservation.service.impl.CarServiceImpl;
+
 public class CarServiceTest {
 
+	private CarService carService;
+	@Mock
+	private CarRepository carRepository;
+	
+	@BeforeEach
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+		carService = new CarServiceImpl(carRepository);
+	}
+	
+	@Test
+	public void getCarByName_ShouldReturnCarDetails() {
+		given(carRepository.findByCarName(anyString())).willReturn(new Car("Toyota", "Bakkie"));
+		
+		Car car = carService.getCarByCarName("Toyota");
+		
+		assertThat(car.getCarName()).isEqualTo("Toyota");
+	}
 }
